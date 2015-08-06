87d8f2eb0179f1a0a774a4e1fbf8daad072a3f68
xd520 <327031193@qq.com>
2015-08-06 08:59:11 +0800

    Initial Commit

---
+++ 添金投/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  添金投
+//
+//  Created by mac on 15/8/6.
+//  Copyright (c) 2015年 ApexSoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
